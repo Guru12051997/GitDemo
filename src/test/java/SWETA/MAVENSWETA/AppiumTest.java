6dcb61f1a8116cb40c1c56f871ca2e10a040a4f6
Guru Prasad Das <gurupdas11@gmail.com>
2020-09-11 17:12:07 +0530

    first commit

---
+++ src/test/java/SWETA/MAVENSWETA/AppiumTest.java
@@ -0,0 +1,18 @@
+package SWETA.MAVENSWETA;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+		@Test
+		public void ElementUI()
+		{
+			System.out.println("ElementUI");
+		}
+		@Test
+		public void MunuTesting()
+		{
+			System.out.println("MunuTesting");
+		}
+	}
+
---
+++ src/test/java/SWETA/MAVENSWETA/RestAPITest.java
@@ -0,0 +1,16 @@
+package SWETA.MAVENSWETA;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void MobileAutomation()
+	{
+		System.out.println("MobileAutomation");
+	}
+	@Test
+	public void FacebookAutomation()
+	{
+		System.out.println("FacebookAutomation");
+	}
+}
